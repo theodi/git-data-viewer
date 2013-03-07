e37424a9a37c99a5b87eae61939581bb19759266
James Smith <james@floppy.org.uk>
2013-03-07 23:41:09 +0000

    Don't initialize when precompiling assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,8 @@ GithubViewer::Application.configure do
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
 
+  config.assets.initialize_on_precompile = false
+  
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
