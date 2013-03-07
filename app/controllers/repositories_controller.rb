2288e6821c7fa449c60d32501dda9a95d17c6756
James Smith <james@floppy.org.uk>
2013-03-07 16:01:37 +0000

    Grab a load of things from git and display

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 class RepositoriesController < ApplicationController
   
   def index
@@ -8,8 +10,17 @@ class RepositoriesController < ApplicationController
     @user, @repo = params[:id].split('/', 2)
     # Get git data
     @repository = $github.repos.get @user, @repo
-    
-    
+    # Get metadata
+    if json = Net::HTTP.get(URI.parse("https://raw.github.com/#{@user}/#{@repo}/master/metadata.json"))
+      @metadata = JSON.parse(json)
+      # Store headers
+      @headers = @metadata['definitions'].map{|x| x['label']}
+      # Get data file
+      if @metadata['data'].is_a?(String)
+        datafile = Net::HTTP.get(URI.parse("https://raw.github.com/#{@user}/#{@repo}/master/#{@metadata['data']}"))
+        @data = CSV.parse(datafile, :headers => true)
+      end
+    end
   end
   
 end
