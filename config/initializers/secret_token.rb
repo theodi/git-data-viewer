1338a9e6a21b441c5621b268714f33c57fed4115
James Smith <james@floppy.org.uk>
2013-03-07 14:40:38 +0000

    Add rails 4b1 framework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GithubViewer::Application.config.secret_key_base = 'ab25aa02386b60b086006e92d15a5b71c780073b88166dc7f47eff3a8c14d395a3415c4a9fc197cdfac87ac12b459a3c5ac8083c65ce69ddd76fe8b3b9f5efc7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GithubViewer::Application.config.session_store :encrypted_cookie_store, key: '_github-viewer_session'
