cfa95d824c2647cedeeafc42f055b309c63c3519
James Smith <james@floppy.org.uk>
2013-03-07 15:48:47 +0000

    Talk to github

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,6 +1,15 @@
 class RepositoriesController < ApplicationController
+  
   def index
   end
+  
   def show
+    # Get user and repository
+    @user, @repo = params[:id].split('/', 2)
+    # Get git data
+    @repository = $github.repos.get @user, @repo
+    
+    
   end
+  
 end
