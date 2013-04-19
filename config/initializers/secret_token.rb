8b579ef564cb784bb1ac8af540425422c8fae18a
James Smith <james@floppy.org.uk>
2013-04-19 18:06:06 +0100

    remove session secret

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-GithubViewer::Application.config.secret_key_base = 'ab25aa02386b60b086006e92d15a5b71c780073b88166dc7f47eff3a8c14d395a3415c4a9fc197cdfac87ac12b459a3c5ac8083c65ce69ddd76fe8b3b9f5efc7'
+GithubViewer::Application.config.secret_key_base = ENV['SESSION_SECRET_GIT_VIEWER'] || SecureRandom.hex(32)
