b7212b0fdafd63a089449ba022643a34ec599b00
James Smith <james@floppy.org.uk>
2013-04-30 13:41:07 +0100

    rename encrypted cookie store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-GithubViewer::Application.config.session_store :encrypted_cookie_store, key: '_github-viewer_session'
+GithubViewer::Application.config.session_store :cookie_store, key: '_github-viewer_session'
