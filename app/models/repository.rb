cab4013fa904cf32d8b1033b4a637a0af6ca9d90
James Smith <james@floppy.org.uk>
2013-05-03 21:01:42 +0100

    DRY up github-specific parts

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -27,12 +27,8 @@ class Repository < ActiveRecord::Base
     (@uri =~ /\A(git|https?):\/\/github\.com\//).present?
   end
 
-  def github_user_name
-    @github_user_name ||= hosted_by_github? ? uri.split('/')[-2] : nil
-  end
-  
-  def github_repository_name
-    @github_repository_name ||= hosted_by_github? ? uri.split('/')[-1].split('.')[0] : nil
+  def github_path(path = '')
+    hosted_by_github? ? "https://github.com/#{github_user_name}/#{github_repository_name}/#{path}" : nil
   end
 
   def commits
@@ -153,6 +149,14 @@ class Repository < ActiveRecord::Base
     rescue ArgumentError
       repo = Git.clone(@uri, working_copy_path)
     end
+  end
+
+  def github_user_name
+    @github_user_name ||= hosted_by_github? ? uri.split('/')[-2] : nil
+  end
+  
+  def github_repository_name
+    @github_repository_name ||= hosted_by_github? ? uri.split('/')[-1].split('.')[0] : nil
   end
 
 end
