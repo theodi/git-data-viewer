8f49f7fd07bd1047da109b1d55b68e3a83808b83
James Smith <james@floppy.org.uk>
2013-05-13 16:12:38 +0100

    fix tests

--- spec/models/repository_spec.rb
+++ spec/models/repository_spec.rb
@@ -3,28 +3,28 @@ require 'spec_helper'
 describe Repository do
 
   before :all do
-    @r = Repository.new(uri: "http://example.com/org/repo.git")
+    @r = Repository.new(uri: "http://github.com/theodi/github-viewer-test-data.git")
   end
 
   it "can generate a stripped version of the uri for descriptions and links" do
-    @r.stripped_uri.should == 'example.com/org/repo'
+    @r.stripped_uri.should == 'github.com/theodi/github-viewer-test-data'
   end
   
   context 'with data on github' do
 
     it 'correctly identified https URLs' do
-      r = Repository.new(uri: "https://github.com/org/repo.git")
-      r.should be_hosted_by_github
+      r = Repository.new(uri: "http://github.com/theodi/github-viewer-test-data.git")
+      r.host.should == :github
     end
     
     it 'correctly identified http URLs' do
-      r = Repository.new(uri: "http://github.com/org/repo.git")
-      r.should be_hosted_by_github
+      r = Repository.new(uri: "http://github.com/theodi/github-viewer-test-data.git")
+      r.host.should == :github
     end
     
     it 'correctly identified git URLs' do
-      r = Repository.new(uri: "git://github.com/org/repo.git")
-      r.should be_hosted_by_github
+      r = Repository.new(uri: "http://github.com/theodi/github-viewer-test-data.git")
+      r.host.should == :github
     end
 
   end
