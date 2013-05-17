2dd665051252110f72eeb8470cb7795ee61275b8
James Smith <james@floppy.org.uk>
2013-05-17 17:14:06 +0100

    document License

--- lib/data_kitten/license.rb
+++ lib/data_kitten/license.rb
@@ -1,9 +1,27 @@
 module DataKitten
 
+  # A license for a {Dataset} or {Distribution}
+  #
   class License
 
-    attr_accessor :id, :name, :uri
+    # @!attribute is
+    #   @return [String] a short ID that identifies the license.
+    attr_accessor :id
+    
+    # @!attribute name
+    #   @return [String] the human name of the license.
+    attr_accessor :name
+    
+    # @!attribute uri
+    #   @return [String] the URI for the license text.
+    attr_accessor :uri
 
+    # Create a new License object.
+    #
+    # @param options [Hash] A set of options with which to initialise the license.
+    # @option options [String] :id the short ID for the license
+    # @option options [String] :name the human name for the license
+    # @option options [String] :uri the URI of the license text
     def initialize(options)
       @id = options[:id]
       @name = options[:name]
