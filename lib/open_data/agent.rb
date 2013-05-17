64c0d68eb63ab05bbf131475ea513c6286a2b212
James Smith <james@floppy.org.uk>
2013-05-17 13:58:50 +0100

    Rename Agent attributes to match FOAF
    and add YARD docs

--- lib/open_data/agent.rb
+++ lib/open_data/agent.rb
@@ -1,15 +1,37 @@
 module OpenData
 
-  # A person or organisation. Named after foaf:Agent but using schema.org naming for internals
+  # A person or organisation.
+  #
+  # Naming is based on {http://xmlns.com/foaf/spec/#term_Agent foaf:Agent}, but with useful aliases for other vocabularies.
   class Agent
 
-    attr_accessor :name, :uri, :email
-
+    # Create a new Agent
+    # 
+    # @param [Hash] options the details of the Agent.
+    # @option options [String] :name The Agent's name
+    # @option options [String] :homepage The homepage URL for the Agent
+    # @option options [String] :mbox Email address for the Agent
+    #
     def initialize(options)
       @name = options[:name]
-      @uri = options[:uri]
-      @email = options[:email]
+      @homepage = options[:homepage]
+      @mbox = options[:mbox]
     end
+
+    # @!attribute name
+    #   @return [String] the name of the Agent
+    attr_accessor :name
+    
+    # @!attribute homepage
+    #   @return [String] the homepage URL of the Agent
+    attr_accessor :homepage
+    alias_method :url, :homepage
+    alias_method :uri, :homepage
+
+    # @!attribute mbox
+    #   @return [String] the email address of the Agent
+    attr_accessor :mbox
+    alias_method :email, :mbox
 
   end  
 
